3860914b6cb017d41f473a892345017a0102783c
Rinkesh5738 <52352858+Rinkesh5738@users.noreply.github.com>
2021-09-14 14:43:59 +0530

    Add files via upload

---
+++ Lab/quiz.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hacking Mania</title>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" type="text/css" href="style2.css">
+</head>
+<body >
+    <center>
+    <header><b>Welcome To Round 1</b></header>
+
+     <form methiod="post">
+     <h2>Click on Start Button To Start Quiz</h2>
+     <br>
+     <a href="https://forms.gle/dpgL3hbFAePLvTfX7" target="_blank">Start</a>
+     </form>
+     </center>
+
+</body>
+</html>
